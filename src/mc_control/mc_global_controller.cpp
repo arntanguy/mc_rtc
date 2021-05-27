e9bc7c1c086932e0362658450ead7eaddbf23f90
Pierre Gergondet <pierre.gergondet@gmail.com>
2021-05-27 19:06:02 +0800

    [mc_rbdyn] Prevent any non-const access to mb/mbc
    This means that any direct setting of the robot's configuration has to
    go through the q/alpha variable

--- src/mc_control/mc_global_controller.cpp
+++ src/mc_control/mc_global_controller.cpp
@@ -222,7 +222,7 @@ void MCGlobalController::init(const std::vector<double> & initq)
     if(q[0].size() == 7)
     {
       const auto & initAttitude = robot().module().default_attitude();
-      q[0] = {std::begin(initAttitude), std::end(initAttitude)};
+      robot().qFloatingBase()->set(Eigen::Map<const Eigen::Matrix<double, 7, 1>>(initAttitude.data()));
       robot().forwardKinematics();
     }
   }
@@ -282,7 +282,6 @@ void MCGlobalController::reset(const std::map<std::string, std::vector<double>> 
 {
   controllers.erase(current_ctrl);
   setup_logger_.erase(current_ctrl);
-  pre_gripper_mbcs_.clear();
   config.load_controllers_configs();
   AddController(current_ctrl);
   controller_ = controllers[current_ctrl].get();
@@ -303,10 +302,10 @@ void MCGlobalController::initEncoders(mc_rbdyn::Robot & robot, const std::vector
   auto & q = robot.mbc().q;
   for(size_t i = 0; i < rjo.size(); ++i)
   {
-    const auto & jn = rjo[i];
-    if(robot.hasJoint(jn))
+    auto jIdx = robot.refJointIndexToQIndex(i);
+    if(jIdx != -1)
     {
-      q[robot.jointIndexByName(jn)][0] = initq[i];
+      robot.q()->set(jIdx, initq[i]);
     }
   }
   for(auto & g : controller_->robot().grippers())
@@ -742,9 +741,9 @@ bool MCGlobalController::run()
       plugin.plugin->before(*this);
       plugin.plugin_before_dt = mc_rtc::clock::now() - start_t;
     }
-    for(size_t i = 0; i < pre_gripper_mbcs_.size() && i < robots.size(); ++i)
+    for(size_t i = 0; i < pre_gripper_qs_.size() && i < robots.size(); ++i)
     {
-      robots[i]->mbc() = pre_gripper_mbcs_[i];
+      robots[i]->q()->set(pre_gripper_qs_[i]);
       robots[i]->forwardKinematics();
     }
 
@@ -756,9 +755,9 @@ bool MCGlobalController::run()
     bool r = controller_->run();
     auto end_controller_run_t = mc_rtc::clock::now();
 
-    pre_gripper_mbcs_.resize(robots.size());
-    std::transform(robots.begin(), robots.end(), pre_gripper_mbcs_.begin(),
-                   [&](const auto & robot) { return robot->mbc(); });
+    pre_gripper_qs_.resize(robots.size());
+    std::transform(robots.begin(), robots.end(), pre_gripper_qs_.begin(),
+                   [&](const auto & robot) { return robot->q()->value(); });
     for(size_t i = 0; i < controller_->robots().size(); ++i)
     {
       const auto & gi = robots[i]->grippers();
