0c9301da75d6329370648b996af66dd51eac90b7
Arnaud Tanguy <arn.tanguy@gmail.com>
2021-12-16 12:28:51 +0100

    [tests] Add an import test for python bindings

---
+++ tests/test_python_bindings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import mc_rbdyn
+import mc_control
+import mc_observers
+import mc_rtc
+import mc_solver
+import mc_tasks
+
+mc_rbdyn.get_robot_module("JVRC1")
