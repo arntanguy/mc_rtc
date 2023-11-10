280a53084dc867b6f7e900ef4ca1eda5d83df92e
Arnaud TANGUY <arn.tanguy@gmail.com>
2023-11-10 15:03:08 +0100

    [GraphViz] Allow to filter by state

--- utils/mc_fsm_graph.cpp
+++ utils/mc_fsm_graph.cpp
@@ -20,12 +20,14 @@ int main(int argc, char * argv[])
   po::variables_map vm;
   po::options_description tool("mc_fsm_graph show options");
   auto config_path = std::vector<std::string>{};
+  auto state = std::string{};
   // clang-format off
   tool.add_options()
     ("help", "Produce this message")
     ("in", po::value<std::string>(), "Input file")
     ("out", po::value<std::string>(), "Output file")
-    ("config_path", po::value<std::vector<std::string>>(&config_path)->multitoken(), "Path to the transitions map");
+    ("config_path", po::value<std::vector<std::string>>(&config_path)->multitoken(), "Path to the transitions map")
+    ("state", po::value<std::string>(&state), "State to display");
   // clang-format on
   po::positional_options_description pos;
   pos.add("in", 1);
@@ -41,11 +43,33 @@ int main(int argc, char * argv[])
   auto in = vm["in"].as<std::string>();
   auto out = vm["out"].as<std::string>();
 
+  if(config_path.size() && state.size())
+  {
+    mc_rtc::log::error("state and config_path options cannot be used simultaneously");
+    exit(-1);
+  }
+
   auto conf = mc_rtc::Configuration(in);
   auto factory = mc_control::fsm::StateFactory(conf("StatesLibraries"), conf("StatesFiles"), false);
+
+  auto states = conf("states", mc_rtc::Configuration{});
+  factory.load(states);
+
   mc_control::fsm::TransitionMap transitions;
   mc_rtc::Configuration c = conf;
-  for(const auto & key : config_path) { c = c(key); }
+  if(config_path.size())
+  {
+    for(const auto & key : config_path) { c = c(key); }
+  }
+  else if(state.size())
+  {
+    if(factory.hasState(state)) { c = factory.configuration(state).config; }
+    else
+    {
+      mc_rtc::log::error("No state named {} in this FSM", state);
+      exit(-1);
+    }
+  }
   transitions.init(factory, c);
   std::ofstream file(out);
   if(file) { transitions.printGraphViz(file); }
